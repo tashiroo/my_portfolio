737565c1cf922fd57893e04ea8a02eee1bbe16ef
taashiroo <ing631tom@outlook.jp>
2021-01-30 15:00:29 +0900

    ボタンの実装

--- uses/_test/main.js
+++ uses/_test/main.js
@@ -1,3 +1,12 @@
-const header_trigger = document.getElementById("header_trigger");
-
-header_trigger.addEventListener("onclick")
+const topBtn= document.getElementById("topBtn");
+const animationFont = document.getElementById("animationFont");
+const downMove1 = document.getElementById("downMove1");
+const downMove2 = document.getElementById("downMove2");
+const downMove3 = document.getElementById("downMove3");
+topBtn.addEventListener("click" ,e =>{
+  topBtn.classList.toggle("animationBtn");
+  animationFont.classList.toggle("animationFont");
+  downMove1.classList.toggle("down");
+  downMove2.classList.toggle("down");
+  downMove3.classList.toggle("down");
+});
