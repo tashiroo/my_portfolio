34e6e636fd0bb7dcd9598b95cd1ce1c99fc4721c
taashiroo <ing631tom@outlook.jp>
2021-02-04 16:49:28 +0900

    コードの調整と画面切り替えの準備

--- uses/_test/main.js
+++ uses/_test/main.js
@@ -1,4 +1,4 @@
-const topBtn= document.getElementById("topBtn");
+  const topBtn= document.getElementById("topBtn");
 const animationFont = document.getElementById("animationFont");
 const downMove1 = document.getElementById("downMove1");
 const downMove2 = document.getElementById("downMove2");
@@ -11,3 +11,26 @@ topBtn.addEventListener("click" ,e =>{
   downMove3.classList.toggle("down");
 });
 
+
+  const home = document.getElementById("home");
+  const main = document.getElementById("main");
+  const form = document.getElementById("form");
+  const goHome = document.getElementById("goHome");
+  const goMain = document.getElementById("goMain");
+  const goForm = document.getElementById("goForm");
+  goMain.addEventListener("click" ,e =>{
+    main.classList.add("front");
+    home.classList.remove("front");
+    form.classList.remove("front");
+  });
+  goHome.addEventListener("click" ,e =>{
+    main.classList.remove("front");
+    home.classList.add("front");
+    form.classList.remove("front");
+  });
+  goForm.addEventListener("click" ,e =>{
+    main.classList.remove("front");
+    home.classList.remove("front");
+    form.classList.add("front");
+  });
+  
