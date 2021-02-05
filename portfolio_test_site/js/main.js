c3ca8f6b58f9f1906b5393932880cad6d6baaa6c
taashiroo <ing631tom@outlook.jp>
2021-02-05 17:17:36 +0900

    画面切り替えの実装とjavascriptの書き換え

--- portfolio_test_site/js/main.js
+++ portfolio_test_site/js/main.js
@@ -9,5 +9,44 @@ topBtn.addEventListener("click" ,e =>{
   downMove1.classList.toggle("down");
   downMove2.classList.toggle("down");
   downMove3.classList.toggle("down");
-  
+});
+
+function reset() {
+  downMove1.classList.remove("down");
+  downMove2.classList.remove("down");
+  downMove3.classList.remove("down");
+  topBtn.classList.remove("animationBtn");
+  animationFont.classList.remove("animationFont");
+ }
+
+
+const header = document.getElementById("header");
+const home = document.getElementById("home");
+const main = document.getElementById("main");
+const form = document.getElementById("form");
+const goHome = document.getElementById("goHome");
+const goMain = document.getElementById("goMain");
+const goMain2 = document.getElementById("goMain2");
+const goForm = document.getElementById("goForm");
+goMain.addEventListener("click" ,e =>{
+  main.classList.remove("hide");
+  header.classList.remove("hide");
+  home.classList.add("hide");
+});
+goMain2.addEventListener("click" ,e =>{
+  header.classList.remove("hide");
+  main.classList.remove("hide");
+  home.classList.add("hide");
+});
+goHome.addEventListener("click" ,e =>{
+  main.classList.add("hide");
+  home.classList.remove("hide");
+  header.classList.add("hide");
+  reset();
+});
+goForm.addEventListener("click" ,e =>{
+  header.classList.add("hide");
+  main.classList.add("hide");
+  form.classList.remove("hide");
+  reset();
 });
--- uses/_test/main.js
+++ uses/_test/main.js
@@ -11,26 +11,43 @@ topBtn.addEventListener("click" ,e =>{
   downMove3.classList.toggle("down");
 });
 
+function reset() {
+  downMove1.classList.remove("down");
+  downMove2.classList.remove("down");
+  downMove3.classList.remove("down");
+  topBtn.classList.remove("animationBtn");
+  animationFont.classList.remove("animationFont");
+ }
 
-  const home = document.getElementById("home");
-  const main = document.getElementById("main");
-  const form = document.getElementById("form");
-  const goHome = document.getElementById("goHome");
-  const goMain = document.getElementById("goMain");
-  const goForm = document.getElementById("goForm");
-  goMain.addEventListener("click" ,e =>{
-    main.classList.add("front");
-    home.classList.remove("front");
-    form.classList.remove("front");
-  });
-  goHome.addEventListener("click" ,e =>{
-    main.classList.remove("front");
-    home.classList.add("front");
-    form.classList.remove("front");
-  });
-  goForm.addEventListener("click" ,e =>{
-    main.classList.remove("front");
-    home.classList.remove("front");
-    form.classList.add("front");
-  });
-  
+
+const header = document.getElementById("header");
+const home = document.getElementById("home");
+const main = document.getElementById("main");
+const form = document.getElementById("form");
+const goHome = document.getElementById("goHome");
+const goMain = document.getElementById("goMain");
+const goMain2 = document.getElementById("goMain2");
+const goForm = document.getElementById("goForm");
+goMain.addEventListener("click" ,e =>{
+  main.classList.remove("front");
+  header.classList.remove("front");
+  home.classList.add("front");
+});
+goMain2.addEventListener("click" ,e =>{
+  header.classList.remove("front");
+  main.classList.remove("front");
+  home.classList.add("front");
+});
+goHome.addEventListener("click" ,e =>{
+  main.classList.add("front");
+  home.classList.remove("front");
+  header.classList.add("front");
+  reset();
+});
+goForm.addEventListener("click" ,e =>{
+  header.classList.add("front");
+  main.classList.add("front");
+  form.classList.remove("front");
+  reset();
+});
+
